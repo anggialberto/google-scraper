1464c4ffd5f19ca88016dc2440b2b64dbfe6fcdf
anggialberto <albertoanggi@gmail.com>
2018-12-31 15:29:30 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 module.exports = (express) => {
   const router = express.Router()
 
-  router.use('/api', require('./GoogleRoutes'))
+  router.use('/api/google', require('./GoogleRoutes'))
 
   return router
 }
